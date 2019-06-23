920406823cf01ab629b85c4c5f00179e0a01f8d0
Li-Bingzi <LiBIngzi_99@icloud.com>
2019-06-23 17:16:26 +0800

    color change completed
    project completed

--- ureca_IK_LiBingzi/Assets/SkinSelect.cs
+++ ureca_IK_LiBingzi/Assets/SkinSelect.cs
@@ -4,26 +4,15 @@ using UnityEngine;
 
 public class SkinSelect : MonoBehaviour
 {
-
-    public GameObject llb, llf, lub, luf, rlb, rlf, rub, ruf;
-    public Renderer rllb, rllf, rlub, rluf, rrlb, rrlf, rrub, rruf;
+    //public Material skin1, skin2;
+    public Material llb, llf, lub, luf, rlb, rlf, rub, ruf;
     public Transform upperarmR, upperarmL, jointR, jointL, handR, handL;
-    int bit = 1;
+
+   // int bit = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        rllb = llb.GetComponent<Renderer>();
-        rllf = llf.GetComponent<Renderer>();
-        rlub = lub.GetComponent<Renderer>();
-        rluf = luf.GetComponent<Renderer>();
-        rrlb = rlb.GetComponent<Renderer>();
-        rrlf = rlf.GetComponent<Renderer>();
-        rrub = rub.GetComponent<Renderer>();
-        rruf = ruf.GetComponent<Renderer>();
-       
-        //rend.enabled = true;
-        //rend.material = skin1;
     }
 
     // Update is called once per frame
@@ -38,56 +27,18 @@ public class SkinSelect : MonoBehaviour
 
         float angleL = Vector3.Angle(upperarmLPos - jointLPos, handLPos - jointLPos);
         float angleR = Vector3.Angle(upperarmRPos - jointRPos, handRPos - jointRPos);
-        //rend.material.shader = Shader.Find("Shirt01");
-        /*if (angleL < 90f)
-        {
-            //set the material to red
-            rllb.material.SetColor('rllb', (0, 0, 1, 1));
 
-        }
-        else
-        {
-            //set the material to blue
+        // smaller angle, lower back and upper front is strained (redder)
+        llb.color = new Color(1, angleL / 180f, angleL / 180f, 1);
+        luf.color = new Color(1, angleL / 180f, angleL / 180f, 1);
+        rlb.color = new Color(1, angleR / 180f, angleR / 180f, 1);
+        ruf.color = new Color(1, angleR / 180f, angleR / 180f, 1);
 
-
-        }
-
-        if (angleR < 90f)
-        {
-            //set the material to red
-
-           
-        }
-        else
-        {
-            //set the material to blue
-
-            
-        }
-
-        // testing
-
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            if(bit == 1)
-            {
-                //set the material to red
-                //rend.material.SetColor("myColor", Color.red);
-                material.color = Color.red;
-                bit = 0;
-
-            }
-            else
-            {
-                //set the material to blue
-                //rend.material.SetColor("myColor", Color.blue);
-                material.color = Color.blue;
-                bit = 1;
-            }
-        }*/
-
-        rllb.material.SetColor('rllb', (angleR/180f, angleR/180f, angleR/180f, 1));
-
+        // smaller angle, upper back and lower front is relaxed (bluer)
+        lub.color = new Color(angleL / 180f, angleL / 180f, 1, 1);
+        llf.color = new Color(angleL / 180f, angleL / 180f, 1, 1);
+        rub.color = new Color(angleR / 180f, angleR / 180f, 1, 1);
+        rlf.color = new Color(angleR / 180f, angleR / 180f, 1, 1);
     }
 
     
